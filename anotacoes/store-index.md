bf7b47e442789865e2a5350ae67286df82f58af5
Luciano Baraúna <baraunaluciano@gmail.com>
2018-08-15 18:17:39 -0300

    primeiro commit

---
+++ anotacoes/store-index.md
@@ -0,0 +1,48 @@
+```
+import Vue from 'vue'
+import Vuex from 'vuex'
+import shop from '@/api/shop'
+import { Promise } from 'core-js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: { // = data
+    products: []
+  },
+  getters: { // = Computed properties
+    // Getter são muito bons para criar calculos ou filtros e são similares as computed properties.
+    // productCount () {
+
+    // },
+    // state = parametro para acessar o state
+    // getters = parametro que diz tudo que existes em getters
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0)
+    }
+  },
+  actions: { // = methods
+    // context = exponhe os métodos do vuex
+    fetchProducts ({commit}) {
+      // make the call
+      // call setProducts mutation
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
+    }
+  },
+  mutations: {
+    // responsável pela atualização dos dados na store.
+    // state = parametro para acessar o state
+    // payload = é o que queremos atualizar.
+    setProducts (state, products) {
+      // update products state
+      state.products = products
+    }
+  }
+})
+
+```
---
+++ src/components/ProductList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <img
+          v-if="loading"
+          src="https://i.imgur.com/JfPpwOA.gif"
+        >
+        <ul v-else>
+            <li v-for="product in products" :key="product.id">
+                {{ product.title }} - {{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+// import store from '@/store/index' removemos o import daqui e colocamos ele globalemnte no main.js
+
+export default {
+  data () {
+    // como temos agora os produtos globalmente na store não precisamos mais de para o caso de products
+    return {
+      loading: false
+      // products: []
+    }
+  },
+  computed: {
+    products () {
+      // return store.state.products // aqui estamos pegando o state global sem o getter
+      // return store.getters.availableProducts // chamada do store no import
+      return this.$store.getters.availableProducts
+    }
+  },
+  created () {
+    // Sem vuex
+    // shop.getProducts(products => {
+    //   // this.products = products // sem vuex
+    //   // store.state.products = products // nunca podemos alterar o state sem chamar uma mutation.
+
+    //   // 1 argumento é o nome da mutation
+    //   // 2 é o payload para ser usado na alteração
+    //   store.commit('setProducts', products)
+    // })
+    //
+    this.loading = true
+    this.$store.dispatch('fetchProducts').then(() => { this.loading = false })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
